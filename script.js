38523b81cff13201b66f18a7fd133be7d83c46a1
Lu <oluchiighodalo@gmail.com>
2025-07-09 19:46:29 -0400

    -added a "toggle dark mode" button

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // Add JavaScript code for your web site here and call it from index.html.
+/*** Dark Mode ***
+  
+  Purpose:
+  - Use this starter code to add a dark mode feature to your website.
+
+  When To Modify:
+  - [ ] Project 5 (REQUIRED FEATURE) 
+  - [ ] Any time after
+***/
+
+// Step 1: Select the theme button
+
+// Step 2: Write the callback function
+const toggleDarkMode = () => {
+    // Write your code here
+    
+    // This section will run whenever the button is clicked
+}
+
+// Step 3: Register a 'click' event listener for the theme button,
+//             and tell it to use toggleDarkMode as its callback function
+
+
+/*** Form Handling [PLACEHOLDER] [ADDED IN UNIT 6] ***/
+/*** Form Validation [PLACEHOLDER] [ADDED IN UNIT 7] ***/
+/*** Animations [PLACEHOLDER] [ADDED IN UNIT 8] ***/
+/*** Success Modal [PLACEHOLDER] [ADDED IN UNIT 9] ***/
