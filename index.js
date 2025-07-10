b64c799f67a1048a3ebe42b2c1b4ac49cd56fb70
Lu <oluchiighodalo@gmail.com>
2025-07-10 19:00:24 -0400

    - code cleanup

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
-// Add JavaScript code for your web site here and call it from index.html.
-/*** Dark Mode ***
-  
-  Purpose:
-  - Use this starter code to add a dark mode feature to your website.
 
-  When To Modify:
-  - [ ] Project 5 (REQUIRED FEATURE) 
-  - [ ] Any time after
-***/
-
-// Step 1: Select the theme button
 let themeButton = document.getElementById('theme-button');
 
-// Step 2: Write the callback function
 const toggleDarkMode = () => {
     document.body.classList.toggle("dark-mode");
-    
-    // This section will run whenever the button is clicked
 }
-
-// Step 3: Register a 'click' event listener for the theme button,
-//             and tell it to use toggleDarkMode as its callback function
 themeButton.addEventListener("click", toggleDarkMode);
