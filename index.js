e31bce2e28b292e5174475bb829f9df1d4a6a3dc
Lu <oluchiighodalo@gmail.com>
2025-07-29 22:39:23 -0400

    - done with error formatting (everything works i think)

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 let themeButton = document.getElementById('theme-button');
-
 const toggleDarkMode = () => {
     document.body.classList.toggle("dark-mode");
 }
 themeButton.addEventListener("click", toggleDarkMode);
 
 let count = 3;
-// Step 1: Add your query for the submit RSVP button here
 const rsvpButton = document.getElementById('rsvp-button');
+const rsvpInputs = document.querySelectorAll("#rsvp-form input");
 
-const addParticipant = (event) => {
-    // Step 2: Write your code to manipulate the DOM here
-
-  event.preventDefault();
-
+const addParticipant = () => {
   const name = document.getElementById('name').value;
   const location = document.getElementById('location').value;
 
   const newParticipant = document.createElement('p');
-
   newParticipant.textContent = `𓂃⋆.˚ ${name} from ${location} has RSVP'd.`;
-
   const participantsList = document.querySelector('.rsvp-participants');
   participantsList.appendChild(newParticipant);
 
@@ -28,18 +21,39 @@ const addParticipant = (event) => {
   if (oldCount) {
       oldCount.remove();
   }
-
-  count=count+1;
-
+  count = count + 1;
   const newCounter = document.createElement('p');
-  newCounter.id = 'rsvp-count'; //
+  newCounter.id = 'rsvp-count';
   newCounter.textContent = "𓂃⋆.˚ " + count + " people have RSVP'd to this event!";
   participantsList.appendChild(newCounter);
-  
-  document.getElementById('name').value = "";
-  document.getElementById('location').value = "";
-  document.getElementById('email').value = "";
-}
+};
 
-// Step 3: Add a click event listener to the submit RSVP button here
-rsvpButton.addEventListener("click", addParticipant);
+const validateForm = (event) => {
+  event.preventDefault(); 
+  let containsErrors = false;
+
+  for (let i = 0; i < rsvpInputs.length; i++) {
+    if (rsvpInputs[i].value.length < 2) {
+      containsErrors = true;
+      rsvpInputs[i].classList.add("error");
+    } else {
+      rsvpInputs[i].classList.remove("error");
+    }
+  }
+
+  const email = document.getElementById('email');
+  if (!email.value.includes('@')) {
+    containsErrors = true;
+    email.classList.add('error');
+  }
+
+  if (containsErrors === false) {
+    addParticipant(); 
+
+    for (let i = 0; i < rsvpInputs.length; i++) {
+      rsvpInputs[i].value = "";
+    }
+  }
+};
+
+rsvpButton.addEventListener("click", validateForm);
